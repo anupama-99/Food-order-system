ebdc1bb197895305d561b6f9360584cc2e97b0f8
Anupana Udeshani <anubwabt414@gmail.com>
2024-02-25 18:41:26 +0530

    Edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './admin/login/login.component';
 import { ProductsComponent } from './admin/products/products.component';
-import { LandingComponent } from './website/landing/landing.component';
+import { LayoutComponent } from './admin/layout/layout.component';
 
 export const routes: Routes = [
 
@@ -16,7 +16,7 @@ export const routes: Routes = [
     },
     {
         path:'',
-        component:LandingComponent,
+        component:LayoutComponent,
         children: [
             {
                 path:'products',
